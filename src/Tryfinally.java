c55ef8658077efe3395f0dd6d0afe97f9bb479a6
1180300410 <646334722@qq.com>
2020-06-28 17:55:57 +0800

    add note

--- src/Tryfinally.java
+++ src/Tryfinally.java
@@ -2,7 +2,7 @@ public class Tryfinally {
     public static void main(String[] args) {
         People bride;
         bride = marry();
-        System.out.println(bride.getState());//结果受finally影响，输出dead
+        System.out.println(bride.getState());//结果受finally影响，输出dead(改变引用无影响，不改变引用改变属性有影响)
     }
     private static People marry() {
         People people = new People();
@@ -13,8 +13,9 @@ public class Tryfinally {
         }catch(Exception e){
             return people;
         }finally {
+//            people.setState("dead");//result: dead
             people = new People();
-            people.setState("haha");
+            people.setState("haha");//result: happy
         }
     }
 }
